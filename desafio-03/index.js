57810c5fcc2f1cb1c2976b8f733c62f5980e815d
Wanda Catellani <cwand07@gmail.com>
2021-06-23 12:44:35 -0300

    add challenge 03

--- desafio-03/index.js
+++ desafio-03/index.js
@@ -1,2 +1,33 @@
 /* JAVASCRIPT ASINCRONICO 
 -------------------------------------------------- */
+
+async function mostrarPalabras(texto, callback) {
+  const textoParrafo = texto.split(" ");
+
+  for (let palabra of textoParrafo) {
+    await new Promise((res) => {
+      setTimeout(() => {
+        res(console.log(palabra));
+      }, 1000);
+    });
+  }
+
+  callback(textoParrafo.length);
+}
+
+async function enviarParrafos() {
+  let totalPalabras = 0;
+
+  const contarPalabras = (cantidadPalabras) => {
+    totalPalabras += cantidadPalabras;
+  };
+
+  await mostrarPalabras(`1. Hola!`, contarPalabras);
+  await mostrarPalabras(`2. Como estas?`, contarPalabras);
+  await mostrarPalabras(`3. Esto es un contador de palabras`, contarPalabras);
+
+  console.log(`Cantidad de palabras: ${totalPalabras}`);
+  console.log(`Proceso completo`);
+}
+
+enviarParrafos();
